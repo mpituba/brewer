56ba9960b1f8c449bafc40caa8f3928df89a2c32
mpituba <mpituba@10.0.10.220>
2021-07-16 10:17:08 -0300

    12.1 Module Pattern e Namespaces
    Modificação do Javascript do brewer.js de forma a ficar modularizado.
    Com padrões de projeto. Ver livro de Addyosmani.

--- brewer/src/main/resources/static/layout/javascripts/brewer.js
+++ brewer/src/main/resources/static/layout/javascripts/brewer.js
@@ -1,8 +1,22 @@
+var Brewer = Brewer || {};
+
+Brewer.MaskMoney = (function(){
+	
+	function MaskMoney() {
+		this.decimal = $('.js-decimal');
+		this.plain = $('.js-plain');
+	}
+	
+	MaskMoney.prototype.enable = function(){
+		this.decimal.maskMoney({decimal: ',', thousands: '.'});
+		this.plain.maskMoney({precision: 0, thousands: '.'});
+	}
+	
+	return MaskMoney;
+	
+}());
+
 $(function(){
-	var decimal = $('.js-decimal');
-	decimal.maskMoney({decimal: ',', thousands: '.'});
-	
-	var plain = $('.js-plain');
-	
-	plain.maskMoney({precision: 0, thousands: '.'});
+	var maskMoney = new Brewer.MaskMoney();
+	maskMoney.enable();
 });
