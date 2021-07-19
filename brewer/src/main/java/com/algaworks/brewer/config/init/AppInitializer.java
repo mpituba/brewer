88a580db9cc0eb8f43044434ec8d32b7aaeb80a9
mpituba <mpituba@10.0.10.220>
2021-07-19 12:02:55 -0300

    14.3 Upload da foto com Ajax

--- brewer/src/main/java/com/algaworks/brewer/config/init/AppInitializer.java
+++ brewer/src/main/java/com/algaworks/brewer/config/init/AppInitializer.java
@@ -1,6 +1,8 @@
 package com.algaworks.brewer.config.init;
 
 import javax.servlet.Filter;
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletRegistration.Dynamic;
 
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
@@ -54,5 +56,8 @@ public class AppInitializer extends AbstractAnnotationConfigDispatcherServletIni
 		return new Filter[] {characterEncodingFilter};
 	}
 	
-
+	@Override
+	protected void customizeRegistration(Dynamic registration) {
+		registration.setMultipartConfig(new MultipartConfigElement(""));
+	}
 }
