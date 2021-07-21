169af02b7ca8067637c1970fb6104cb849ee1d45
Marcelo Pituba <68237153+mpituba@users.noreply.github.com>
2021-07-21 15:18:58 -0300

    14.8 Mostrando a foto na tela
    Handlebars, Javascript, Html.

--- brewer/src/main/java/com/algaworks/brewer/controller/FotosController.java
+++ brewer/src/main/java/com/algaworks/brewer/controller/FotosController.java
@@ -1,6 +1,8 @@
 package com.algaworks.brewer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -32,5 +34,14 @@ public class FotosController {
 				
 		return resultado;
 	}
+	
+	@GetMapping("/temp/{nome:.*}")
+	public byte[] recuperarFotoTemporaria(@PathVariable String nome) {
+		return fotoStorage.recuperarFotoTemporaria(nome);
+	}
+	
 
 }
+
+
+
