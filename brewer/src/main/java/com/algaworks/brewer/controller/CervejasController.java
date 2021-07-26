8a1b5c6b43472b23ad9a3b25ff551d259deed724
Marcelo Pituba <68237153+mpituba@users.noreply.github.com>
2021-07-26 17:36:32 -0300

    15.7 Paginação na pesquisa de cervejas
    Com exemplo de paginação simples no início do vídeo.

--- brewer/src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ brewer/src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -5,6 +5,8 @@ package com.algaworks.brewer.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -64,13 +66,18 @@ public class CervejasController {
 	}
 	
 	@GetMapping
-	public ModelAndView pesquisar(CervejaFilter cervejaFilter, BindingResult result) {
+	public ModelAndView pesquisar(CervejaFilter cervejaFilter, 
+						BindingResult result, @PageableDefault(size = 2) Pageable pageable) {
 		ModelAndView mv = new ModelAndView("cerveja/PesquisaCervejas");
 		mv.addObject("estilos", estilos.findAll());
 		mv.addObject("sabores", Sabor.values());
 		mv.addObject("origens", Origem.values());
 		
-		mv.addObject("cervejas", cervejas.filtrar(cervejaFilter));
+		System.out.println(">>>>> PageNumber : " + pageable.getPageNumber());
+		System.out.println(">>>>> PageSize   : " + pageable.getPageSize());
+		
+		
+		mv.addObject("cervejas", cervejas.filtrar(cervejaFilter, pageable));
 		return mv;
 	}
 	
--- brewer/src/main/java/com/algaworks/brewer/repository/helper/cerveja/CervejasImpl.java
+++ brewer/src/main/java/com/algaworks/brewer/repository/helper/cerveja/CervejasImpl.java
@@ -9,6 +9,7 @@ import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.criterion.MatchMode;
 import org.hibernate.criterion.Restrictions;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
@@ -24,8 +25,16 @@ public class CervejasImpl implements CervejasQueries{
 	@SuppressWarnings("unchecked")
 	@Override
 	@Transactional(readOnly = true)
-	public List<Cerveja> filtrar(CervejaFilter filtro){
+	public List<Cerveja> filtrar(CervejaFilter filtro, Pageable pageable){
 		Criteria criteria = manager.unwrap(Session.class).createCriteria(Cerveja.class);
+		
+		int paginaAtual = pageable.getPageNumber();
+		int totalRegistrosPorPagina = pageable.getPageSize();
+		int primeiroRegistro = paginaAtual * totalRegistrosPorPagina;
+		
+		criteria.setFirstResult(primeiroRegistro);
+		criteria.setMaxResults(totalRegistrosPorPagina);
+		
 		if (filtro != null) {
 			if (!StringUtils.isEmpty(filtro.getSku())) {
 				criteria.add(Restrictions.eq("sku", filtro.getSku()));
@@ -59,10 +68,18 @@ public class CervejasImpl implements CervejasQueries{
 		return criteria.list();
 	}
 	
+	@Override
+	public List<Cerveja> filtrar(CervejaFilter filtro) {
+
+		return null;
+	}
 	
 	private boolean isEstiloPresente(CervejaFilter filtro) {
 		return filtro.getEstilo() != null && filtro.getEstilo().getCodigo() != null;
 	}
+
+
+
 }
 
 
