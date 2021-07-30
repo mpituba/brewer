255cb19949a805cd2027b4e293de5fa10bcd1515
Marcelo Pituba <68237153+mpituba@users.noreply.github.com>
2021-07-30 15:52:37 -0300

    16.4. Implementando radio TipoPessoa
    Utiliza dados de uma Enum que é recebido na página via ModelAndView.
    Também utiliza o Thymeleaf Extras Data Attribute.

--- brewer/src/main/java/com/algaworks/brewer/config/WebConfig.java
+++ brewer/src/main/java/com/algaworks/brewer/config/WebConfig.java
@@ -29,6 +29,7 @@ import org.thymeleaf.templateresolver.ITemplateResolver;
 import com.algaworks.brewer.controller.CervejasController;
 import com.algaworks.brewer.controller.converter.EstiloConverter;
 import com.algaworks.brewer.thymeleaf.BrewerDialect;
+import com.github.mxab.thymeleaf.extras.dataattribute.dialect.DataAttributeDialect;
 
 import nz.net.ultraq.thymeleaf.LayoutDialect;
 
@@ -96,6 +97,11 @@ public class WebConfig extends WebMvcConfigurerAdapter implements ApplicationCon
 		 * está em www.thymeleaf.org/documentation.html  #Extending Thymeleaf.
 		 */
 		engine.addDialect(new BrewerDialect());
+		
+		/**
+		 * Layout Dialect usado pelo Thymeleaf Extras Data Attribute
+		 */
+		engine.addDialect(new DataAttributeDialect());
 		
 		return engine;
 	}
--- brewer/src/main/java/com/algaworks/brewer/controller/ClientesController.java
+++ brewer/src/main/java/com/algaworks/brewer/controller/ClientesController.java
@@ -7,21 +7,26 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.brewer.model.Cliente;
+import com.algaworks.brewer.model.TipoPessoa;
 
 @Controller
+@RequestMapping("/clientes")
 public class ClientesController {
 	
-	@RequestMapping("/clientes/novo")
-	public String novo(Cliente cliente) {
-		return "cliente/CadastroCliente";
+	@RequestMapping("/novo")
+	public ModelAndView novo(Cliente cliente) {
+		ModelAndView mv = new ModelAndView("cliente/CadastroCliente");
+		mv.addObject("tiposPessoa", TipoPessoa.values());
+		return mv;
 	}
 	
 	//Quando for feito um POST em clientes/novo chamará este método 
-	@RequestMapping(value = "clientes/novo", method = RequestMethod.POST)
-	public String cadastrar(@Valid Cliente cliente, BindingResult result, Model model,
+	@RequestMapping(value = "/novo", method = RequestMethod.POST)
+	public ModelAndView cadastrar(@Valid Cliente cliente, BindingResult result, Model model,
 			RedirectAttributes attributes) {
 		
 		//Verificador do BindingResult
@@ -30,7 +35,7 @@ public class ClientesController {
 		}
 		
 		attributes.addFlashAttribute("mensagem", "Cliente salvo com sucesso!");
-		return "redirect:/clientes/novo";
+		return new ModelAndView ("redirect:/clientes/novo");
 	}
 	
 	
