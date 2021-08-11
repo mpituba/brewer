100387da191b64c7d38da192f6f99efb14ee64a4
Marcelo Pituba <68237153+mpituba@users.noreply.github.com>
2021-08-11 18:10:44 -0300

    17.3. Cache profissional com Guava do Google.

--- brewer/src/main/java/com/algaworks/brewer/config/WebConfig.java
+++ brewer/src/main/java/com/algaworks/brewer/config/WebConfig.java
@@ -2,12 +2,14 @@ package com.algaworks.brewer.config;
 
 import java.math.BigDecimal;
 import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.beans.BeansException;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.cache.config.CacheManagementConfigUtils;
+import org.springframework.cache.guava.GuavaCacheManager;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
@@ -36,6 +38,7 @@ import com.algaworks.brewer.controller.converter.EstadoConverter;
 import com.algaworks.brewer.controller.converter.EstiloConverter;
 import com.algaworks.brewer.thymeleaf.BrewerDialect;
 import com.github.mxab.thymeleaf.extras.dataattribute.dialect.DataAttributeDialect;
+import com.google.common.cache.CacheBuilder;
 
 import nz.net.ultraq.thymeleaf.LayoutDialect;
 
@@ -173,7 +176,18 @@ public class WebConfig extends WebMvcConfigurerAdapter implements ApplicationCon
 	 */
  	@Bean
  	public CacheManager cacheManager() {
- 		return new ConcurrentMapCacheManager();
+ 		
+ 		//Configuração das opções do CacheManager
+ 		CacheBuilder<Object, Object> cacheBuilder = CacheBuilder.newBuilder()
+ 				.maximumSize(3)
+ 				.expireAfterAccess(20, TimeUnit.SECONDS);
+ 		
+ 		//Instancia o CacheManager
+ 		GuavaCacheManager cacheManager = new GuavaCacheManager();
+ 		
+ 		cacheManager.setCacheBuilder(cacheBuilder);
+ 		
+ 		return cacheManager;
  	}
  	
 
