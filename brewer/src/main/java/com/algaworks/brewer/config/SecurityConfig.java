c846ced126b5b893d92f4a440fbc39762ebac9fc
Marcelo Pituba <mpituba@yahoo.com>
2021-09-20 17:22:41 -0300

    19.2. Configurando o Spring Security no Projeto Inserida uma tela de login com csrf desabilitado 	I

--- brewer/src/main/java/com/algaworks/brewer/config/SecurityConfig.java
+++ brewer/src/main/java/com/algaworks/brewer/config/SecurityConfig.java
@@ -1,12 +1,39 @@
 package com.algaworks.brewer.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
-public class SecurityConfig {
+/*
+ * Configuração do Springframework Web Security
+ * */
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	/*Form de login*/
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		//Config da tela de login em memória
+		auth.inMemoryAuthentication()
+			.withUser("admin").password("admin").roles("CADASTRO_CLIENTE");
+	}
+	
+	
+	/*Autoriza  e gera form de logout*/
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.anyRequest().authenticated()
+				.and()
+			.formLogin()
+				.and()
+			.csrf().disable();
+	}
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
---
+++ brewer/src/main/java/com/algaworks/brewer/config/init/SecurityInitializer.java
@@ -0,0 +1,8 @@
+package com.algaworks.brewer.config.init;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/*Inicializador do Spring Security*/
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer{
+	
+}
