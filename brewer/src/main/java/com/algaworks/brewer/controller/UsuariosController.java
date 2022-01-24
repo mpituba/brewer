2830e4b7140fe41419d44a1006320556f87cd81a
Marcelo Pituba <mpituba@yahoo.com>
2022-01-24 17:22:02 -0300

    21.6. Paginação da pesquisa de usuários

--- brewer/src/main/java/com/algaworks/brewer/controller/UsuariosController.java
+++ brewer/src/main/java/com/algaworks/brewer/controller/UsuariosController.java
@@ -1,10 +1,14 @@
 package com.algaworks.brewer.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 //import java.util.Arrays;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -17,6 +21,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.algaworks.brewer.controller.page.PageWrapper;
 import com.algaworks.brewer.model.Usuario;
 import com.algaworks.brewer.repository.Grupos;
 import com.algaworks.brewer.repository.Usuarios;
@@ -82,10 +87,14 @@ public class UsuariosController {
 	}
 	
 	@GetMapping
-	public ModelAndView pesquisar(UsuarioFilter usuarioFilter) {
+	public ModelAndView pesquisar(UsuarioFilter usuarioFilter, @PageableDefault(size=5) Pageable pageable, HttpServletRequest httpServletRequest) {
 		ModelAndView mv = new ModelAndView("/usuario/PesquisaUsuarios");
-		mv.addObject("usuarios", usuarios.filtrar(usuarioFilter));
 		mv.addObject("grupos", grupos.findAll());
+		
+		PageWrapper<Usuario> paginaWrapper = new PageWrapper<>(usuarios.filtrar(usuarioFilter, pageable)
+				, httpServletRequest);
+		mv.addObject("pagina", paginaWrapper);
+		
 		return mv;
 	}
 	
--- brewer/src/main/java/com/algaworks/brewer/repository/helper/usuario/UsuariosQueries.java
+++ brewer/src/main/java/com/algaworks/brewer/repository/helper/usuario/UsuariosQueries.java
@@ -3,6 +3,9 @@ package com.algaworks.brewer.repository.helper.usuario;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.algaworks.brewer.model.Usuario;
 import com.algaworks.brewer.repository.filter.UsuarioFilter;
 
@@ -12,5 +15,5 @@ public interface UsuariosQueries {
 	
 	public List<String> permissoes(Usuario usuario);
 	
-	public List<Usuario> filtrar(UsuarioFilter filtro);
+	public Page<Usuario> filtrar(UsuarioFilter filtro, Pageable pageable);
 }
