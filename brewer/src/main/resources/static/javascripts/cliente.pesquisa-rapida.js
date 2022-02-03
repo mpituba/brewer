702b88e0acc5e37b77e423232cc3a48f0d9b9753
Marcelo Pituba <mpituba@yahoo.com>
2022-02-03 15:13:20 -0300

    22.4. Selecionando o cliente na pesquisa rápida

--- brewer/src/main/resources/static/javascripts/cliente.pesquisa-rapida.js
+++ brewer/src/main/resources/static/javascripts/cliente.pesquisa-rapida.js
@@ -14,8 +14,16 @@ Brewer.PesquisaRapidaCliente = (function() {
 	
 	PesquisaRapidaCliente.prototype.iniciar = function(){
 		this.pesquisaRapidaBtn.on('click', onPesquisaRapidaClicado.bind(this));
+		this.pesquisaRapidaClientesModal.on('shown.bs.modal', onModalShow.bind(this));
 			
 	}
+	
+	
+	function onModalShow() {
+		this.nomeInput.focus();		
+		
+	}
+	
 	
 	function onPesquisaRapidaClicado(event) {
 		event.preventDefault();
@@ -33,9 +41,12 @@ Brewer.PesquisaRapidaCliente = (function() {
 	}
 	
 	function onPesquisaConcluida(resultado) {
+		this.mensagemErro.addClass('hidden');
 		var html = this.template(resultado);
 		this.containerTabelaPesquisa.html(html);
-		this.mensagemErro.addClass('hidden');
+		
+		var tabelaClientePesquisaRapida = new Brewer.TabelaClientePesquisaRapida(this.pesquisaRapidaClientesModal);
+		tabelaClientePesquisaRapida.iniciar();
 	}
 	
 	function onErroPesquisa() {
@@ -44,6 +55,29 @@ Brewer.PesquisaRapidaCliente = (function() {
 	
 	return PesquisaRapidaCliente;
 	
+}());
+
+Brewer.TabelaClientePesquisaRapida = (function() {
+	
+	function TabelaClientePesquisaRapida(modal) {
+		this.modalCliente = modal;
+		this.cliente = $('.js-cliente-pesquisa-rapida');
+	}
+	
+	TabelaClientePesquisaRapida.prototype.iniciar = function() {
+		this.cliente.on('click', onClienteSelecionado.bind(this));
+	}	
+	
+	function onClienteSelecionado(evento) {
+		this.modalCliente.modal('hide');
+		
+		var clienteSelecionado = $(evento.currentTarget);
+		$('#nomeCliente').val(clienteSelecionado.data('nome'));
+		$('#codigoCliente').val(clienteSelecionado.data('codigo'));
+	}
+	
+	return TabelaClientePesquisaRapida;
+
 }());
 
 $(function() {
