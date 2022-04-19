275ea75b4c7907508187bb5a4d4d40dbc1d8ab31
Marcelo Pituba <mpituba@yahoo.com>
2022-04-19 10:08:06 -0300

    23.4. Testando a tabela de itens de venda Implantação de testes JUnit.

--- brewer/src/main/java/com/algaworks/brewer/venda/TabelaItensVenda.java
+++ brewer/src/main/java/com/algaworks/brewer/venda/TabelaItensVenda.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.algaworks.brewer.model.Cerveja;
 import com.algaworks.brewer.model.ItemVenda;
 
 public class TabelaItensVenda {
@@ -14,7 +15,16 @@ public class TabelaItensVenda {
 		return itens.stream()
 				.map(ItemVenda::getValorTotal)
 				.reduce(BigDecimal::add)
-				.orElse(BigDecimal.TEN);
+				.orElse(BigDecimal.ZERO);
+	}
+	
+	public void adicionarItem(Cerveja cerveja, Integer quantidade) {
+		ItemVenda itemVenda = new ItemVenda();
+		itemVenda.setCerveja(cerveja);
+		itemVenda.setQuantidade(quantidade);
+		itemVenda.setValorUnitario(cerveja.getValor());
+		
+		itens.add(itemVenda);
 	}
 
 }
